be000ecec8e094f9e16ed73d47b39b8106851683
pahae-tests <wahipahae@gmail.com>
2025-12-21 21:35:31 +0100

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,15 @@
-import styles from '../styles/globals.css'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Tasky</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
